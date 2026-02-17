d2a5af40b51bdc16eedfbd06bf3149e3d96c0a46
pradeep <durgapradeep.kodavati@vvtsolutions.in>
2026-02-17 18:34:25 +0530

    Fix CORS: allow all origins so frontend on :3000 can call API on :8000

--- main.py
+++ main.py
@@ -67,16 +67,13 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "*",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
